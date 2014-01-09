f2890cab6c5057568add7bb64512323a24b0bd0b
Vincent Van Driessche <vncntvandriessche@gmail.com>
2014-01-09 18:18:26 +0100

    Puts the required libraries into rc/ subfolder

---
+++ rc/required_libraries.lua
@@ -0,0 +1,15 @@
+--------------------------------------------------------------------------------
+-- Required libraries
+--------------------------------------------------------------------------------
+gears 	        = require("gears")
+awful           = require("awful")
+awful.rules     = require("awful.rules")
+awful.autofocus = require("awful.autofocus")
+wibox           = require("wibox")
+beautiful       = require("beautiful")
+naughty         = require("naughty")
+vicious         = require("vicious")
+scratch         = require("scratch")
+
+-- Enables the menubar that runs through applications folder
+menubar = require("menubar")
