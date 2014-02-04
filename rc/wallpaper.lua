aabb69e3e9ea406a44a9628197b849e17fb97da8
Vincent Van Driessche <vncntvandriessche@gmail.com>
2014-02-04 12:39:43 +0100

    Less forks, I hope

--- rc/wallpaper.lua
+++ rc/wallpaper.lua
@@ -3,8 +3,10 @@
 ----------------------------------------------------------------------------------
 
 -- Maximize wallpaper at every screen
-if beautiful.wallpaper then
-    for s = 1, screen.count() do
-        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
-    end
+if not beautiful.wallpaper then
+        return
+end
+
+for s = 1, screen.count() do
+    gears.wallpaper.maximized(beautiful.wallpaper, s, true)
 end
